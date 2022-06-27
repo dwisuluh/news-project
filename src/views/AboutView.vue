fb5ea4fce03ddff14da4753543154ee01598c27e
dwisuluh <dwi.suluh@students.utdi.ac.id>
2022-06-27 22:26:05 +0700

    up app form

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about pa-6">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi laboriosam aliquam impedit suscipit sed explicabo. Dolorum commodi temporibus ea, eligendi esse, repellat alias, quae dignissimos aperiam eveniet quos harum tenetur.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi laboriosam aliquam impedit suscipit sed explicabo. Dolorum commodi temporibus ea, eligendi esse, repellat alias, quae dignissimos aperiam eveniet quos harum tenetur.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi laboriosam aliquam impedit suscipit sed explicabo. Dolorum commodi temporibus ea, eligendi esse, repellat alias, quae dignissimos aperiam eveniet quos harum tenetur.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi laboriosam aliquam impedit suscipit sed explicabo. Dolorum commodi temporibus ea, eligendi esse, repellat alias, quae dignissimos aperiam eveniet quos harum tenetur.</p>
   </div>
 </template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,163 +1,109 @@
 <template>
   <div class="login pa-4">
     <h1>This is an Login page</h1>
-    <validation-observer
-    ref="observer"
-    v-slot="{ invalid }"
-  >
-    <form @submit.prevent="submit">
-      <validation-provider
-        v-slot="{ errors }"
-        name="Name"
-        rules="required|max:10"
-      >
-        <v-text-field
-          v-model="name"
-          :counter="10"
-          :error-messages="errors"
-          label="Name"
-          required
-        ></v-text-field>
-      </validation-provider>
-      <validation-provider
-        v-slot="{ errors }"
-        name="phoneNumber"
-        :rules="{
-          required: true,
-          digits: 7,
-          regex: '^(71|72|74|76|81|82|84|85|86|87|88|89)\\d{5}$'
-        }"
-      >
-        <v-text-field
-          v-model="phoneNumber"
-          :counter="7"
-          :error-messages="errors"
-          label="Phone Number"
-          required
-        ></v-text-field>
-      </validation-provider>
-      <validation-provider
-        v-slot="{ errors }"
-        name="email"
-        rules="required|email"
-      >
-        <v-text-field
-          v-model="email"
-          :error-messages="errors"
-          label="E-mail"
-          required
-        ></v-text-field>
-      </validation-provider>
-      <validation-provider
-        v-slot="{ errors }"
-        name="select"
-        rules="required"
-      >
-        <v-select
-          v-model="select"
-          :items="items"
-          :error-messages="errors"
-          label="Select"
-          data-vv-name="select"
-          required
-        ></v-select>
-      </validation-provider>
-      <validation-provider
-        v-slot="{ errors }"
-        rules="required"
-        name="checkbox"
-      >
-        <v-checkbox
-          v-model="checkbox"
-          :error-messages="errors"
-          value="1"
-          label="Option"
-          type="checkbox"
-          required
-        ></v-checkbox>
-      </validation-provider>
-
-      <v-btn
-        class="mr-4"
-        type="submit"
-        :disabled="invalid"
-      >
-        submit
-      </v-btn>
-      <v-btn @click="clear">
-        clear
-      </v-btn>
-    </form>
-  </validation-observer>
+    <v-row justify="center">
+      <v-col cols="12" sm="10" md="8" lg="6">
+        <v-card ref="form">
+          <v-card-text>
+            <v-text-field ref="name" v-model="name" :rules="[() => !!name || 'This field is required']"
+              :error-messages="errorMessages" label="Full Name" placeholder="John Doe" required></v-text-field>
+            <v-text-field ref="address" v-model="address" :rules="[
+              () => !!address || 'This field is required',
+              () => !!address && address.length <= 25 || 'Address must be less than 25 characters',
+              addressCheck
+            ]" label="Address Line" placeholder="Snowy Rock Pl" counter="25" required></v-text-field>
+            <v-text-field ref="city" v-model="city" :rules="[() => !!city || 'This field is required', addressCheck]"
+              label="City" placeholder="El Paso" required></v-text-field>
+            <v-text-field ref="state" v-model="state" :rules="[() => !!state || 'This field is required']"
+              label="State/Province/Region" required placeholder="TX"></v-text-field>
+            <v-text-field ref="zip" v-model="zip" :rules="[() => !!zip || 'This field is required']"
+              label="ZIP / Postal Code" required placeholder="79938"></v-text-field>
+            <v-autocomplete ref="country" v-model="country" :rules="[() => !!country || 'This field is required']"
+              :items="countries" label="Country" placeholder="Select..." required></v-autocomplete>
+          </v-card-text>
+          <v-divider class="mt-12"></v-divider>
+          <v-card-actions>
+            <v-btn text>
+              Cancel
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-slide-x-reverse-transition>
+              <v-tooltip v-if="formHasErrors" left>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn icon class="my-0" v-bind="attrs" @click="resetForm" v-on="on">
+                    <v-icon>mdi-refresh</v-icon>
+                  </v-btn>
+                </template>
+                <span>Refresh form</span>
+              </v-tooltip>
+            </v-slide-x-reverse-transition>
+            <v-btn color="primary" text @click="submit">
+              Submit
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
-
 <script>
-//   import { required, digits, email, max, regex } from 'vee-validate/dist/rules'
+export default {
+  data: () => ({
+    countries: ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua &amp; Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia &amp; Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Cape Verde', 'Cayman Islands', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Cook Islands', 'Costa Rica', 'Cote D Ivoire', 'Croatia', 'Cruise Ship', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'French West Indies', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kuwait', 'Kyrgyz Republic', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nepal', 'Netherlands', 'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russia', 'Rwanda', 'Saint Pierre &amp; Miquelon', 'Samoa', 'San Marino', 'Satellite', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'St Kitts &amp; Nevis', 'St Lucia', 'St Vincent', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', `Timor L'Este`, 'Togo', 'Tonga', 'Trinidad &amp; Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks &amp; Caicos', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Venezuela', 'Vietnam', 'Virgin Islands (US)', 'Yemen', 'Zambia', 'Zimbabwe'],
+    errorMessages: '',
+    name: null,
+    address: null,
+    city: null,
+    state: null,
+    zip: null,
+    country: null,
+    formHasErrors: false,
+  }),
 
-import Vue from 'vue'
-
-//   import { extend, ValidationObserver, ValidationProvider, setInteractionMode } from 'vee-validate'
-//  Vue.use(VeeValidate)
-
-  setInteractionMode('eager')
-
-  extend('digits', {
-    ...digits,
-    message: '{_field_} needs to be {length} digits. ({_value_})',
-  })
-
-  extend('required', {
-    ...required,
-    message: '{_field_} can not be empty',
-  })
-
-  extend('max', {
-    ...max,
-    message: '{_field_} may not be greater than {length} characters',
-  })
-
-  extend('regex', {
-    ...regex,
-    message: '{_field_} {_value_} does not match {regex}',
-  })
-
-  extend('email', {
-    ...email,
-    message: 'Email must be valid',
-  })
-
-  export default {
-    components: {
-      ValidationProvider,
-      ValidationObserver,
+  computed: {
+    form() {
+      return {
+        name: this.name,
+        address: this.address,
+        city: this.city,
+        state: this.state,
+        zip: this.zip,
+        country: this.country,
+      }
     },
-    data: () => ({
-      name: '',
-      phoneNumber: '',
-      email: '',
-      select: null,
-      items: [
-        'Item 1',
-        'Item 2',
-        'Item 3',
-        'Item 4',
-      ],
-      checkbox: null,
-    }),
+  },
 
-    methods: {
-      submit () {
-        this.$refs.observer.validate()
-      },
-      clear () {
-        this.name = ''
-        this.phoneNumber = ''
-        this.email = ''
-        this.select = null
-        this.checkbox = null
-        this.$refs.observer.reset()
-      },
+  watch: {
+    name() {
+      this.errorMessages = ''
     },
-  }
+  },
+
+  methods: {
+    addressCheck() {
+      this.errorMessages = this.address && !this.name
+        ? `Hey! I'm required`
+        : ''
+
+      return true
+    },
+    resetForm() {
+      this.errorMessages = []
+      this.formHasErrors = false
+
+      Object.keys(this.form).forEach(f => {
+        this.$refs[f].reset()
+      })
+    },
+    submit() {
+      this.formHasErrors = false
+
+      Object.keys(this.form).forEach(f => {
+        if (!this.form[f]) this.formHasErrors = true
+
+        this.$refs[f].validate(true)
+      })
+    },
+  },
+}
 </script>
